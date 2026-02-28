3ab7650de654017b73bbd564be1a6d5fa63cc7bb
Vitalii Samofal <vitalii.samofal@softkit.dev>
2026-02-28 16:52:50 +0000

    Add project metadata: LICENSE, `.npmignore`, `.gitattributes`, and configure `pnpm-lock.yaml`.

---
+++ .releaserc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    'main',
+    'next',
+    'next-major',
+    {
+      name: 'beta',
+      prerelease: true
+    },
+    {
+      name: 'alpha',
+      prerelease: true
+    }
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"]
+      }
+    ],
+    "@semantic-release/github",
+  ],
+  preset: "angular"
+}
